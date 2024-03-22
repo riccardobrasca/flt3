db8bfa0362ff4b6faf7e5ccc40bb09c5197f3821
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-03-22 10:18:01 +0100

    first commit

---
+++ Flt3.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `Flt3` library.
+-- Import modules here that should be built as part of the library.
+import «Flt3».Basic
---
+++ lakefile.lean
@@ -0,0 +1,33 @@
+import Lake
+open Lake DSL
+
+def moreServerArgs := #[
+  "-Dpp.unicode.fun=true", -- pretty-prints `fun a ↦ b`
+  "-Dpp.proofs.withType=false"
+]
+
+-- These settings only apply during `lake build`, but not in VSCode editor.
+def moreLeanArgs := moreServerArgs
+
+-- These are additional settings which do not affect the lake hash,
+-- so they can be enabled in CI and disabled locally or vice versa.
+-- Warning: Do not put any options here that actually change the olean files,
+-- or inconsistent behavior may result
+def weakLeanArgs : Array String :=
+  if get_config? CI |>.isSome then
+    #["-DwarningAsError=true"]
+  else
+    #[]
+
+package «flt3» where
+  moreServerArgs := moreServerArgs
+  -- add any package configuration options here
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git" @ "RB/CIRM"
+
+@[default_target]
+lean_lib «Flt3» where
+  moreLeanArgs := moreLeanArgs
+  weakLeanArgs := weakLeanArgs
+  -- add any library configuration options here
