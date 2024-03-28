b432299b55807a8f5b210a62117d56f2a4746bd7
David Lowry-Duda <david@lowryduda.com>
2024-03-28 10:20:35 +0100

    Complete proof of `final`

--- FLT3/FLT3.lean
+++ FLT3/FLT3.lean
@@ -1002,21 +1002,23 @@ lemma by_kummer : ↑S.u₄ ∈ ({1, -1} : Finset (𝓞 K)) := by
 
 lemma final : S.Y ^ 3 + (S.u₄ * S.Z) ^ 3 = S.u₅ * (λ ^ (S.multiplicity - 1) * S.X) ^ 3 := by
   rw [show S.Y ^ 3 + (S.u₄ * S.Z) ^ 3 = S.Y ^ 3 + S.u₄^2 * S.u₄ * S.Z ^ 3 by ring]
-  have goal := formula2 S
-  rw [show Y S ^ 3 + ↑(u₄ S) * Z S ^ 3 = Y S ^ 3 + 1 * ↑(u₄ S) * Z S ^ 3 by ring] at goal
-  suffices hyp : S.u₄ = 1 ∨ S.u₄ = -1 by
+  have f2 := formula2 S
+  rw [show Y S ^ 3 + ↑(u₄ S) * Z S ^ 3 = Y S ^ 3 + 1 * ↑(u₄ S) * Z S ^ 3 by ring] at f2
+  suffices hyp : S.u₄ = (1 : 𝓞 K)  ∨ S.u₄ = (-1 : 𝓞 K) by
     rcases hyp with (h | h)
     · have hh : S.u₄ ^ 2 = (1 : 𝓞 K) := by
         rw [h]
         simp
-      nth_rewrite 1 [← hh] at goal
-      exact goal
+      nth_rewrite 1 [← hh] at f2
+      exact f2
     · have hh : S.u₄ ^ 2 = (1 : 𝓞 K) := by
         rw [h]
         simp
-      nth_rewrite 1 [← hh] at goal
-      exact goal
-  sorry
+      nth_rewrite 1 [← hh] at f2
+      exact f2
+  have simple_kummer := by_kummer S
+  simp only [Finset.mem_insert, Finset.mem_singleton] at simple_kummer
+  exact simple_kummer
 
 noncomputable
 def _root_.Solution'_final : Solution' where
